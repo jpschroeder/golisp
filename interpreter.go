ede61824527a358c22264b2a250f9042bfc7121f
John Schroeder <john@schroederspace.com>
2021-07-13 23:50:14 -0400

    all the way to fib

--- interpreter.go
+++ interpreter.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
-
-// todo: less duplication when iterating lists vs slices
 
 var defaultEnv map[Symbol]interface{}
 
@@ -14,10 +13,18 @@ func init() {
 		Symbol("-"):     primitive(sub),
 		Symbol("*"):     primitive(mul),
 		Symbol("/"):     primitive(div),
+		Symbol("="):     primitive(eq),
+		Symbol("<"):     primitive(lt),
+		Symbol("<="):    primitive(lte),
+		Symbol(">"):     primitive(gt),
+		Symbol(">="):    primitive(gte),
 		Symbol("list"):  primitive(list),
-		Symbol("def"):   specialform(def),
 		Symbol("quote"): specialform(quote),
+		Symbol("do"):    specialform(do),
+		Symbol("def"):   specialform(def),
 		Symbol("fn"):    specialform(fn),
+		Symbol("defn"):  specialform(defn),
+		Symbol("if"):    specialform(ifprim),
 	}
 }
 
@@ -109,34 +116,20 @@ func evalMap(val map[interface{}]interface{}, env *Env) (map[interface{}]interfa
 	return ret, nil
 }
 
-func agg(args []interface{}, accumInt func(int, int) int, accumFloat func(float64, float64) float64) (interface{}, error) {
-	var ret interface{}
-	for idx, arg := range args {
-		if idx == 0 {
-			ret = arg
-			continue
-		}
-
-		ri, rIsInt := ret.(int)
-		rf, rIsFloat := ret.(float64)
-
-		ai, aIsInt := arg.(int)
-		af, aIsFloat := arg.(float64)
-
-		if rIsInt && aIsInt {
-			ret = accumInt(ri, ai)
-		} else if rIsInt && aIsFloat {
-			ret = accumFloat(float64(ri), af)
-		} else if rIsFloat && aIsInt {
-			ret = accumFloat(rf, float64(ai))
-		} else if rIsFloat && aIsFloat {
-			ret = accumFloat(rf, af)
-		} else {
-			return nil, fmt.Errorf("Invalid operand: %v", arg)
-		}
+func apply(proc procedure, args []interface{}) (interface{}, error) {
+	if len(args) != len(proc.params) {
+		return nil, fmt.Errorf("Wrong number of args (%d) passed to procedure", len(args))
 	}
-	return ret, nil
+
+	child := ChildEnv(proc.env)
+	for i, arg := range args {
+		child.Define(proc.params[i], arg)
+	}
+
+	return do(proc.body, child)
 }
+
+// Primitives
 
 func add(args []interface{}) (interface{}, error) {
 	return agg(args,
@@ -178,18 +171,146 @@ func div(args []interface{}) (interface{}, error) {
 		})
 }
 
-func list(args []interface{}) (interface{}, error) {
-	if len(args) == 1 {
-		return args[0], nil
+func agg(args []interface{}, accumInt func(int, int) int, accumFloat func(float64, float64) float64) (interface{}, error) {
+	if len(args) < 1 {
+		return nil, fmt.Errorf("Wrong number of args (%d) passed to procedure", len(args))
 	}
+
+	ret := args[0]
+	for i := 1; i < len(args); i++ {
+		ri, rIsInt := ret.(int)
+		rf, rIsFloat := ret.(float64)
+
+		ai, aIsInt := args[i].(int)
+		af, aIsFloat := args[i].(float64)
+
+		if rIsInt && aIsInt {
+			ret = accumInt(ri, ai)
+		} else if rIsInt && aIsFloat {
+			ret = accumFloat(float64(ri), af)
+		} else if rIsFloat && aIsInt {
+			ret = accumFloat(rf, float64(ai))
+		} else if rIsFloat && aIsFloat {
+			ret = accumFloat(rf, af)
+		} else {
+			return nil, fmt.Errorf("Invalid operand: %v", args[i])
+		}
+	}
+	return ret, nil
+}
+
+func lt(args []interface{}) (interface{}, error) {
+	return order(args,
+		func(r, x int) bool {
+			return r < x
+		},
+		func(r, x float64) bool {
+			return r < x
+		})
+}
+
+func lte(args []interface{}) (interface{}, error) {
+	return order(args,
+		func(r, x int) bool {
+			return r <= x
+		},
+		func(r, x float64) bool {
+			return r <= x
+		})
+}
+
+func gt(args []interface{}) (interface{}, error) {
+	return order(args,
+		func(r, x int) bool {
+			return r > x
+		},
+		func(r, x float64) bool {
+			return r > x
+		})
+}
+
+func gte(args []interface{}) (interface{}, error) {
+	return order(args,
+		func(r, x int) bool {
+			return r >= x
+		},
+		func(r, x float64) bool {
+			return r >= x
+		})
+}
+
+func order(args []interface{}, orderInt func(int, int) bool, orderFloat func(float64, float64) bool) (interface{}, error) {
+	if len(args) < 1 {
+		return nil, fmt.Errorf("Wrong number of args (%d) passed to procedure", len(args))
+	}
+
+	for i := 1; i < len(args); i++ {
+		ri, rIsInt := args[i-1].(int)
+		rf, rIsFloat := args[i-1].(float64)
+
+		ai, aIsInt := args[i].(int)
+		af, aIsFloat := args[i].(float64)
+
+		var ret bool
+		if rIsInt && aIsInt {
+			ret = orderInt(ri, ai)
+		} else if rIsInt && aIsFloat {
+			ret = orderFloat(float64(ri), af)
+		} else if rIsFloat && aIsInt {
+			ret = orderFloat(rf, float64(ai))
+		} else if rIsFloat && aIsFloat {
+			ret = orderFloat(rf, af)
+		} else {
+			return nil, fmt.Errorf("Invalid operand: %v", args[i])
+		}
+
+		if !ret {
+			return false, nil
+		}
+	}
+	return true, nil
+}
+
+func eq(args []interface{}) (interface{}, error) {
+	if len(args) < 1 {
+		return nil, fmt.Errorf("Wrong number of args (%d) passed to: =", len(args))
+	}
+
+	compare := args[0]
+	for i := 1; i < len(args); i++ {
+		if reflect.TypeOf(compare) != reflect.TypeOf(args[i]) {
+			return false, nil
+		}
+		if !Equals(compare, args[i]) {
+			return false, nil
+		}
+	}
+	return true, nil
+}
+
+func list(args []interface{}) (interface{}, error) {
 	return args, nil
 }
+
+// Special Forms
 
 func quote(args []interface{}, env *Env) (interface{}, error) {
 	if len(args) != 1 {
 		return nil, fmt.Errorf("Wrong number of args (%d) passed to quote", len(args))
 	}
 	return args[0], nil
+}
+
+func do(args []interface{}, env *Env) (interface{}, error) {
+	if len(args) == 0 {
+		return nil, nil
+	}
+
+	ret, err := evalList(args, env)
+	if err != nil {
+		return nil, err
+	}
+	return ret[len(ret)-1], nil
 }
 
 func def(args []interface{}, env *Env) (interface{}, error) {
@@ -213,31 +334,6 @@ func def(args []interface{}, env *Env) (interface{}, error) {
 
 	env.Define(sym, evaled)
 	return sym, nil
-}
-
-func apply(proc procedure, args []interface{}) (interface{}, error) {
-	if len(args) > len(proc.params) {
-		return nil, fmt.Errorf("Too many parameters to procedure")
-	}
-
-	child := ChildEnv(proc.env)
-	for i, arg := range args {
-		child.Define(proc.params[i], arg)
-	}
-
-	if len(args) < len(proc.params) {
-		return procedure{
-			params: proc.params[len(args):],
-			body:   proc.body,
-			env:    child,
-		}, nil
-	}
-
-	ret, err := evalList(proc.body, child)
-	if err != nil {
-		return nil, err
-	}
-	return ret[len(ret)-1], nil
 }
 
 func fn(args []interface{}, env *Env) (interface{}, error) {
@@ -264,4 +360,48 @@ func fn(args []interface{}, env *Env) (interface{}, error) {
 		body:   args[1:],
 		env:    env,
 	}, nil
+}
+
+func defn(args []interface{}, env *Env) (interface{}, error) {
+	if len(args) < 2 {
+		return nil, fmt.Errorf("Too few arguments to defn")
+	}
+
+	proc, err := fn(args[1:], env)
+	if err != nil {
+		return nil, err
+	}
+	return def([]interface{}{args[0], proc}, env)
+}
+
+func ifprim(args []interface{}, env *Env) (interface{}, error) {
+	if len(args) < 2 {
+		return nil, fmt.Errorf("Too few arguments to if")
+	}
+	if len(args) > 3 {
+		return nil, fmt.Errorf("Too many arguments to if")
+	}
+
+	cond, err := Eval(args[0], env)
+	if err != nil {
+		return nil, err
+	}
+
+	if isTruthy(cond) {
+		return Eval(args[1], env)
+	}
+
+	if len(args) == 3 {
+		return Eval(args[2], env)
+	}
+
+	return nil, nil
+}
+
+func isTruthy(val interface{}) bool {
+	isTrue, isBoolean := val.(bool)
+	if isBoolean {
+		return isTrue
+	}
+	return val != nil
 }
--- interpreter_test.go
+++ interpreter_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"io"
 	"reflect"
 	"strings"
 	"testing"
@@ -17,10 +18,6 @@ func TestArithmetic(t *testing.T) {
 	testEval(t, "(/ (- (+ 515 (* -87 311)) 296) 27)", -994)
 }
 
-func TestError(t *testing.T) {
-	testEvalError(t, "(abc 1 2 3)")
-}
-
 func TestEmpty(t *testing.T) {
 	testEval(t, "()", newList())
 	testEval(t, "[]", newVect())
@@ -31,6 +28,98 @@ func TestCollections(t *testing.T) {
 	testEval(t, "[1 2 (+ 1 2)]", newVect(1, 2, 3))
 	testEval(t, "{\"a\" (+ 7 8)}", newMap{"a": 15})
 	testEval(t, "{:a (+ 7 8)}", newMap{Keyword("a"): 15})
+	testEval(t, "(list 1)", newList(1))
+	testEval(t, "(list 1 2 (+ 1 2))", newList(1, 2, 3))
+	testEval(t, "(quote (\"a\" (+ 7 8)))", newList("a", newList(Symbol("+"), 7, 8)))
+}
+
+func TestEq(t *testing.T) {
+	testEval(t, "(= 1 2)", false)
+	testEval(t, "(= 1 1)", true)
+	testEval(t, "(= 1 1.1)", false)
+	testEval(t, "(= \"blah\" \"bloo\")", false)
+	testEval(t, "(= \"blah\" \"blah\")", true)
+	testEval(t, "(= [1 2] [1 2])", true)
+	testEval(t, "(= {1 2 3 4} {1 2 3 5})", false)
+	testEval(t, "(= {1 2 3 4} {1 2 3 4})", true)
+}
+
+func TestOrder(t *testing.T) {
+	testEval(t, "(< 1)", true)
+	testEval(t, "(< 1 2 3 10)", true)
+	testEval(t, "(< 10 20 30 15)", false)
+	testEval(t, "(< 10 50 30 40)", false)
+	testEval(t, "(< 50 20 30 40)", false)
+	testEval(t, "(>= 1)", true)
+	testEval(t, "(>= 10 9 8 8.0 -1 -2.5)", true)
+	testEval(t, "(>= 0 9 8 8 -1 -2.5)", false)
+	testEval(t, "(>= 10 9 8 8.5 -1 -2.5)", false)
+}
+
+func TestDo(t *testing.T) {
+	testEval(t, "(do)", nil)
+	testEval(t, "(do 1)", 1)
+	testEval(t, "(do (+ 1 2) (+ 3 4))", 7)
+}
+
+func TestDef(t *testing.T) {
+	testEval(t, "(def x 10)", Symbol("x"))
+	testEval(t, "(do (def x 10) x)", 10)
+	testEval(t, "(do (def x (+ 10 5)) (+ x 7))", 22)
+}
+
+func TestFn(t *testing.T) {
+	testEval(t, "((fn [x] (+ 1 x)) 10)", 11)
+	testEval(t, "((fn [x y] (+ y x)) 10 7)", 17)
+}
+
+func TestDefn(t *testing.T) {
+	testEval(t, `(do
+		(defn add1 [x] (+ 1 x))
+		(add1 5))`, 6)
+	testEval(t, `(do
+		(defn addxy [x y] (+ y x))
+		(addxy 10 7))`, 17)
+}
+
+func TestIf(t *testing.T) {
+	testEval(t, "(if true 1)", 1)
+	testEval(t, "(if false 1)", nil)
+	testEval(t, "(if true 1 2)", 1)
+	testEval(t, "(if false 1 2)", 2)
+	testEval(t, "(if (= 1 1) \"trueval\" 2)", "trueval")
+	testEval(t, "(if (= 1 2) \"trueval\" 2)", 2)
+	testEval(t, "(if \"blah\" 1 2)", 1)
+	testEval(t, "(if nil 1 2)", 2)
+}
+
+func TestFib(t *testing.T) {
+	testEval(t, `
+		(defn fib [n]
+			(if (< n 2) 
+				n
+				(+ (fib (- n 1)) (fib (- n 2)))))
+		(fib 10)`, 55)
+	testEval(t, `
+		(defn fib [n]
+			(defn fib-iter [curr next n]
+				(if (= n 0)
+					curr
+					(fib-iter next (+ curr next) (- n 1))))
+			(fib-iter 0 1 n))
+		(fib 10)`, 55)
+}
+
+func TestError(t *testing.T) {
+	testEvalError(t, "(abc 1 2 3)")
+	testEvalError(t, "((fn [x y] (+ y x)) 10 7 8)")
+	testEvalError(t, "((fn [x y] (+ y x)) 10)")
+	testEvalError(t, "(def)")
+	testEvalError(t, "(def x)")
+	testEvalError(t, "(def \"x\" 1)")
+	testEvalError(t, "(if)")
+	testEvalError(t, "(if true)")
+	testEvalError(t, "(if true 1 2 3)")
 }
 
 func testEval(t *testing.T, input string, output interface{}) {
@@ -42,7 +131,8 @@ func testEval(t *testing.T, input string, output interface{}) {
 		return
 	}
 	if !Equals(actual, output) {
-		t.Errorf("\nExpected: %v - %v\nActual: %v - %v\n",
+		t.Errorf("\nExpr: %s\nExpected: %v - %v\nActual: %v - %v\n",
+			input,
 			reflect.TypeOf(output), Print(output),
 			reflect.TypeOf(actual), Print(actual))
 	}
@@ -55,13 +145,20 @@ func testEvalError(t *testing.T, input string) {
 	}
 }
 
-func readEval(input string, env *Env) (interface{}, error) {
+func readEval(input string, env *Env) (val interface{}, err error) {
 	in := bufio.NewReader(strings.NewReader(input))
 
-	val, err := Read(in)
-	if err != nil {
-		return "", err
+	for {
+		rval, rerr := Read(in)
+		if rerr == io.EOF {
+			break
+		}
+		if rerr != nil {
+			return "", err
+		}
+
+		val, err = Eval(rval, env)
 	}
 
-	return Eval(val, env)
+	return
 }
